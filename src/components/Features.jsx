ae9b0d76d1545cd73e75e85587f71d52d9500e0d
Fernando <fervalle200190@gmail.com>
2022-02-07 16:34:38 -0400

    brief changes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -11,43 +11,53 @@ const Features = () => {
                          your devices so you can access them on the go{" "}
                     </p>
                     <div className="slider-selector">
-                        <label htmlFor="slider1">
-                            Simple Bookmarking
-                            <div className="orange-underline"></div>
-                        </label>
-                        <label htmlFor="slider2">
-                            Speedy Searching
-                            <div className="orange-underline"></div>
-                        </label>
-                        <label htmlFor="slider3">
-                            Easy Sharing
-                            <div className="orange-underline"></div>
-                        </label>
+                         <label htmlFor="slider1">
+                              Simple Bookmarking
+                              <div className="orange-underline"></div>
+                         </label>
+                         <label htmlFor="slider2">
+                              Speedy Searching
+                              <div className="orange-underline"></div>
+                         </label>
+                         <label htmlFor="slider3">
+                              Easy Sharing
+                              <div className="orange-underline"></div>
+                         </label>
                     </div>
                </div>
                <div className="slider-features-container">
                     <div className="inner-slider">
-                         <input type="radio" name="slider-radio" id="slider1" />
-                         <input type="radio" name="slider-radio" id="slider2" />
-                         <input type="radio" name="slider-radio" id="slider3" />
+                         <input type="radio" name="slider-radio" id="slider1" value={"info1"} />
+                         <input type="radio" name="slider-radio" id="slider2" value={"info2"}/>
+                         <input type="radio" name="slider-radio" id="slider3" value={"info3"}/>
 
                          <div className="slide">
-                              Bookmark in one click Organize your bookmarks
-                              however you like. Our simple drag-and-drop
-                              interface gives you complete control over how you
-                              manage your favourite sites. More Info
+                              <h3>Bookmark in one click</h3>
+                              <p>
+                                   Organize your bookmarks however you like. Our
+                                   simple drag-and-drop interface gives you
+                                   complete control over how you manage your
+                                   favourite sites. More Info
+                              </p>
                          </div>
                          <div className="slide">
-                              Intelligent search Our powerful search feature
-                              will help you find saved sites in no time at all.
-                              No need to trawl through all of your bookmarks.
-                              More Info
+                              <h3>Intelligent search</h3>
+                              <p>
+                                   {" "}
+                                   Our powerful search feature will help you
+                                   find saved sites in no time at all. No need
+                                   to trawl through all of your bookmarks. More
+                                   Info
+                              </p>
                          </div>
                          <div className="slide">
-                              Share your bookmarks Easily share your bookmarks
-                              and collections with others. Create a shareable
-                              link that you can send at the click of a button.
-                              More Info
+                              <h3>Share your bookmarks</h3>
+                              <p>
+                                   {" "}
+                                   Easily share your bookmarks and collections
+                                   with others. Create a shareable link that you
+                                   can send at the click of a button. More Info
+                              </p>
                          </div>
                     </div>
                </div>
